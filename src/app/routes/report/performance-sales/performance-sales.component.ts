d795463589a20998fe721e7504e01a491e4f8187
Fajar <fajar.ramdh09@gmail.com>
2017-08-07 17:02:52 +0700

    Tenants & Performance Sales Chart Page

--- src/app/routes/report/performance-sales/performance-sales.component.ts
+++ src/app/routes/report/performance-sales/performance-sales.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../../../services/data.service';
+import $ from "jquery";
 
 @Component({
   selector: 'app-performance-sales',
@@ -7,7 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerformanceSalesComponent implements OnInit {
 
-  constructor() { }
+  public dataSales:any = [this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000)];;
+  public datasetsSales:any = [{
+    label: 'Spending',
+    data: this.dataSales
+  }];
+  public categoriesLabels: any = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public chartOption:any = {
+    scaleShowGridLines: false,
+    responsive: false,
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false,
+          drawOnChartArea: false
+        },
+      }],
+      yAxes: [{
+        gridLines: {
+          display: false,
+          drawOnChartArea: false
+        },
+      }]
+    }
+  };
+  public chartType: any = {
+    bar: 'bar',
+    line: 'line',
+    horizontalBar: 'horizontalBar'
+  };
+
+  clickChart(evt){
+    console.log('chart clicked !')
+    const chart = $('#sales-chart');
+    const activePoints = chart.getElementsAtEvent(evt);
+    const chartData = activePoints[0]['_chart'].config.data;
+    const idx = activePoints[0]['_index'];
+    const label = chartData.labels[idx];
+    const value = chartData.datasets[0].data[idx];
+    const url = "http://example.com/?label=" + label + "&value=" + value;
+    alert(url);
+  }
+  constructor(public dataservice: DataService) {
+    const chart = $('#sales-chart');
+    // TODO : Add drilldown request
+  }
 
   ngOnInit() {
   }
--- src/app/routes/report/tenants/tenants.component.ts
+++ src/app/routes/report/tenants/tenants.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../../../services/data.service';
 
 @Component({
   selector: 'app-tenants',
@@ -7,7 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TenantsComponent implements OnInit {
 
-  constructor() { }
+  public tenantData1: any = [this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000)];
+  public tenantData2: any = [this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000), this.dataservice.getRandomNumber(500000, 2000000)];
+  public tenantTR: any = [this.dataservice.getRandomNumber(5000000, 20000000), this.dataservice.getRandomNumber(5000000, 20000000), this.dataservice.getRandomNumber(5000000, 20000000), this.dataservice.getRandomNumber(5000000, 20000000), this.dataservice.getRandomNumber(5000000, 20000000), this.dataservice.getRandomNumber(5000000, 20000000), this.dataservice.getRandomNumber(5000000, 20000000),];
+  public memberSpending: any = this.tenantData1;
+  public comparisonData: any = [this.dataservice.getRandomNumber(7, 12), this.dataservice.getRandomNumber(7, 12), this.dataservice.getRandomNumber(7, 12), this.dataservice.getRandomNumber(7, 12), this.dataservice.getRandomNumber(7, 12), this.dataservice.getRandomNumber(7, 12), this.dataservice.getRandomNumber(7, 12),];
+  public dayLabels: any = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'];
+  public tenantLabels: any = [ 'KFC', 'McDonalds', 'H&M', 'Retro', 'Carrefour', 'Gramedia', 'Pepper Lunch'];
+  public chartType: any = {
+    bar: 'bar',
+    line: 'line',
+    horizontalBar: 'horizontalBar'
+  };
+  public datasetsTenant1: any = [{
+    data: this.tenantData1,
+    label: 'Spending',
+  }];
+  public datasetsTenant2: any = [{
+    data: this.tenantData2,
+    label: 'Spending',
+  }];
+  public datasetsTr: any = [{
+    data: this.tenantTR,
+    label: 'TR record',
+  }];
+  public datasetsMember: any = [{
+    data: this.memberSpending,
+    label: 'Member Spending'
+  }];
+  public datasetsTrMember: any = [{
+    data: this.comparisonData,
+    label: 'TR Record VS Memebr Spending',
+    fill: false,
+  }];
+
+  public ChartOptions: any = {
+    scaleShowGridLines: false,
+    responsive: false,
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false,
+          drawOnChartArea: false
+        },
+      }],
+      yAxes: [{
+        gridLines: {
+          display: false,
+          drawOnChartArea: false
+        },
+      }]
+    }
+  };
+
+  constructor(public dataservice: DataService) {
+    console.log(this.datasetsTenant1)
+  }
 
   ngOnInit() {
   }
